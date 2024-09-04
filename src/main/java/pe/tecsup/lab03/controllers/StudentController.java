124ad0a96f9f8637680bc59011be710d26d30db2
ramdom <ramdom@abc>
2024-09-04 17:23:12 -0500

    first commit

---
+++ src/main/java/pe/tecsup/lab03/controllers/StudentController.java
@@ -0,0 +1,12 @@
+package pe.tecsup.lab03.controllers;
+
+public class StudentController {
+
+    public String name;
+    public String lastName;
+
+    public String getInfo(){
+        return name + lastName;
+    }
+
+}
